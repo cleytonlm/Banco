3b7b97c95450e81a3e969f539269b39ac8858bb2
Treinamento <Treinamento@RD723939.raiadrogasil.com>
2020-02-12 11:47:52 -0300

    Exercicio Banco Santander

--- src/ExercicioBanco/Poupança.java
+++ src/ExercicioBanco/Poupança.java
@@ -1,28 +1,29 @@
 package ExercicioBanco;
 
 public class Poupança extends Conta {
-        private double saldo;
+    private double saldo;
+    private double juros;
 
-        public void recolherjuros(double dias) {
-            dias = saldo + (saldo*1.2/100);
-            this.saldo += dias;
-        }
-
-        public void deposito(double dinheiro) {
-            if (dinheiro > 0) {
-                this.saldo += dinheiro;
-                System.out.println("Deposito realizado com sucesso" + saldo);
-            } else {
-                System.out.println("valor invalido para deposito !");
-            }
-        }
-
-        public void saque(double dinheiro) {
-            if (dinheiro > saldo) {
-                System.out.println("voce nao tem dinheiro");
-            } else
-                this.saldo -= dinheiro;
-            System.out.println("Saque realizado com sucesso Seu novo saldo é " + saldo);
-        }
-
+    public void recolherjuros() {
+        juros = saldo + (saldo * 1.2 / 100);
+        this.saldo += juros;
     }
+
+    public void deposito(double dinheiro) {
+        if (dinheiro > 0) {
+            this.saldo += dinheiro;
+            System.out.println("Deposito realizado com sucesso" + saldo);
+        } else {
+            System.out.println("valor invalido para deposito !");
+        }
+    }
+
+    public void saque(double dinheiro) {
+        if (dinheiro > saldo) {
+            System.out.println("voce nao tem dinheiro");
+        } else
+            this.saldo -= dinheiro;
+        System.out.println("Saque realizado com sucesso Seu novo saldo é " + saldo);
+    }
+
+}
